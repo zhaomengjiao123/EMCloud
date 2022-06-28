71b338cea063ed74da84d7abc16b008f7aa2f965
Tang-Shaw <57103846+Tang-Shaw@users.noreply.github.com>
2022-06-28 21:07:28 +0800

    Add files via upload

---
+++ EMCloud-client/src/components/equipment/Equipment.vue
@@ -0,0 +1,246 @@
+<template>
+  <div>
+    <el-table
+      :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+      style="width: 100%">
+      <el-table-column
+        label="产品"
+        width="180">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ scope.row.product_name}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="客户"
+        width="180">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.company_name}}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column label="设备id" v-if="false" prop="equipment_id"></el-table-column>
+      <el-table-column label="公司id" v-if="false" prop="company_id"></el-table-column>
+      <el-table-column label="产品id" v-if="false" prop="product_id"></el-table-column>
+      <el-table-column label="销售员id" v-if="false" prop="salesman_id"></el-table-column>
+      <el-table-column label="设备编码" prop="equipment_number"></el-table-column>
+      <el-table-column label="销售人" prop="salesman_name"></el-table-column>
+      <el-table-column label="销售时间" prop="sell_time"></el-table-column>
+      <el-table-column label="是否在线1/0" prop="equipment_state"></el-table-column>
+      <el-table-column label="备注" prop="equipment_comment"></el-table-column>
+      <el-table-column align="right">
+        <template slot="header" slot-scope="scope">
+          <el-input
+            v-model="search"
+            size="mini"
+            placeholder="输入关键字搜索"/>
+        </template>
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-popconfirm
+            confirm-button-text='好的'
+            cancel-button-text='不用了'
+            icon="el-icon-info"
+            icon-color="red"
+            title="这是一段内容确定删除吗？"
+            @confirm="handleDelete(scope.$index, scope.row)"
+          >
+            <el-button slot="reference" size="mini" type="danger">删除</el-button>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-button style="margin: 10px 0px;"  @click="saveUserInfo" type="success" size="mini">添加</el-button>
+    <transition name="el-zoom-in-center">
+      <div v-show="show" class="transition-box">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="产品">
+            <el-input v-model="form.product_name"></el-input>
+          </el-form-item>
+          <el-form-item label="客户">
+            <el-input v-model="form.company_name"></el-input>
+          </el-form-item>
+          <el-form-item label="产品id">
+            <el-input v-model="form.product_id"></el-input>
+          </el-form-item>
+          <el-form-item label="设备id">
+            <el-input v-model="form.equipment_id"></el-input>
+          </el-form-item>
+          <el-form-item label="公司id">
+            <el-input v-model="form.company_id"></el-input>
+          </el-form-item>
+          <el-form-item label="销售员id">
+            <el-input v-model="form.salesman_id"></el-input>
+          </el-form-item>
+          <el-form-item label="设备编码">
+            <el-input v-model="form.equipment_number"></el-input>
+          </el-form-item>
+          <el-form-item label="销售人员">
+            <el-input v-model="form.salesman_name"></el-input>
+          </el-form-item>
+          <el-form-item label="销售时间">
+            <!--        <el-input v-model="form.sell_time"></el-input>-->
+            <div class="block">
+              <span class="demonstration"></span>
+              <el-date-picker
+                v-model="form.sell_time"
+                type="date"
+                value-format="yyyy-MM-dd"
+                placeholder="选择日期">
+              </el-date-picker>
+            </div>
+          </el-form-item>
+          <el-form-item label="是否在线">
+            <!--            <el-input v-model="form.equipment_state"></el-input>-->
+            <template>
+              <el-radio v-model="form.equipment_state" label="1">在线</el-radio>
+              <el-radio v-model="form.equipment_state" label="0">宕机</el-radio>
+            </template>
+          </el-form-item>
+          <el-form-item label="备注信息">
+            <el-input v-model="form.equipment_comment"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">立即创建</el-button>
+            <el-button type="primary" @click="onEdit">编辑</el-button>
+            <el-button @click="saveUserInfo">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "machine",
+  data() {
+    return {
+      tableData: [],
+      search:"",
+      show:false,
+      form: {
+        product_name: '',
+        company_name: '',
+        product_id: '',
+        equipment_id: '',
+        company_id: '',
+        salesman_id: '',
+        equipment_number: '',
+        salesman_name: '',
+        sell_time: '',
+        equipment_state: '',
+        equipment_comment:'',
+      }
+    }
+  },
+  methods: {
+    saveUserInfo(){
+      this.show=true;
+      this.form={};
+    },
+    handleEdit(index, row) {
+      console.log(index, row);
+      this.show=true;
+      this.form=row;
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+      console.log(row.equipment_id);
+      this.$http.get("http://121.5.74.11:8080/equipment/deleteEquipment/detail",{params:{"equipment_id":row.equipment_id}}).then(res=>{
+        if(res.data.status){
+          this.$message({
+            message: res.data.msg,
+            type: 'success'
+          });
+          this.findAllTableData();
+        }else{
+          this.$message.error(res.data.msg);
+        }
+      });
+    },
+    onSubmit() {
+      let userData = new URLSearchParams();
+      userData.append('product_id', this.form.product_id)
+      // userData.append('product_name', this.form.product_name)
+      userData.append('company_id', this.form.company_id)
+      //userData.append('company_name', this.form.company_name)
+      userData.append('equipment_number', this.form.equipment_number)
+      //userData.append('salesman_name', this.form.salesman_name)
+      userData.append('salesman_id', this.form.salesman_id)
+      userData.append('sell_time', this.form.sell_time)
+      //原方法
+      userData.append('equipment_state', this.form.equipment_state)
+
+      //判断form.equipment_state是在线or宕机，传入接口1or0
+      //  if (this.form.equipment_state.isEqual("在线"))
+      // userData.append('equipment_state', 1)
+      // else if (this.form.equipment_state.isEqual("宕机"))
+      //   userData.append('equipment_state', 0)
+      userData.append('equipment_comment', this.form.equipment_comment)
+      this.$http.post("http://121.5.74.11:8080/equipment/addEquipment",userData).then(res=>{
+        if(res.data.status){
+          this.$message({
+            message: '恭喜你，'+res.data.msg,
+            type: 'success'
+          });
+          //清空表单信息
+          this.form={};
+          //隐藏表单
+          this.show=false;
+          this.findAllTableData();
+        }else{
+          this.$message.error(res.data.msg);
+        }
+      });
+    },
+    onEdit(){
+      console.log(this.form.company_id);
+      this.$http.post("http://121.5.74.11:8080/equipment/update",this.form).then(res=>{
+        console.log(res.data);
+        if(res.data.status){
+          this.$message({
+            message: '恭喜你，'+res.data.msg,
+            type: 'success'
+          });
+          //清空表单信息
+          this.form={};
+          //隐藏表单
+          this.show=false;
+          this.findAllTableData();
+        }else{
+          this.$message.error(res.data.msg);
+        }
+      });
+    },
+    findAllTableData(){
+      // console.log(row.equipment_id);
+      //product = []
+      this.$http.get("http://121.5.74.11:8080/equipment/getAll").then(res=>{
+        this.tableData=res.data;
+
+      });
+    }
+  },
+  created(){
+    this.findAllTableData();
+  }
+}
+</script>
+
+<style scoped>
+.transition-box {
+  margin-bottom: 10px;
+  width: 100%;
+  height: 600px;
+  border-radius: 4px;
+  padding: 40px 20px;
+  box-sizing: border-box;
+  margin-right: 20px;
+}
+</style>
