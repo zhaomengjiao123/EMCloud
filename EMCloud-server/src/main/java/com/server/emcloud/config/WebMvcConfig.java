0f0b27723b49b493efc939acc9303946b8dabb4b
wangjunbo <447758919@qq.com>
2022-07-11 23:46:07 +0800

    文件上传与访问

--- EMCloud-server/src/main/java/com/server/emcloud/config/WebMvcConfig.java
+++ EMCloud-server/src/main/java/com/server/emcloud/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.server.emcloud.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -22,4 +23,5 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
+
 }
